87ac2c23708a21500b6a75d71316c6641f258c66
BrunodePaula <brunodepaula18@gmail.com>
2020-03-08 21:14:01 -0300

    Update - Apis

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
-});
+routes.post('/sessions', SessionController.store);
+
+// Middlewares
+routes.use(authMiddlewares);
+
+// Users
+routes.post('/users', UserController.store);
+routes.put('/users/:id', UserController.update);
+routes.get('/users', UserController.get);
 
 export default routes;
