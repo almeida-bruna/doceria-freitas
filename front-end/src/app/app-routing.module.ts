6947300f4c6cf8c39406a36154fb8465521fd4b5
almeida-bruna <33481897+almeida-bruna@users.noreply.github.com>
2020-03-11 17:10:01 -0300

    Arrumando a parte de roteamento

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 import { PromocoesComponent } from './pages/promocoes/promocoes.component';
+import { NovidadesComponent } from './pages/novidades/novidades.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SobreAEmpresaComponent } from './pages/sobre-a-empresa/sobre-a-empresa.component';
 
 
 const routes: Routes = [
-  { path: 'promocoes', component: PromocoesComponent }
+  { path: '', component: HomeComponent},
+  { path: 'promocoes', component: PromocoesComponent },
+  { path: 'novidades', component: NovidadesComponent},
+  { path: 'sobre_a_empresa', component: SobreAEmpresaComponent}
 ];
 
 @NgModule({
