fca0e243222daa50ed40369edbbc20cddff21859
almeida-bruna <33481897+almeida-bruna@users.noreply.github.com>
2020-05-27 03:31:01 -0300

    Arrumando produto

--- front-end/src/app/pages/home/home.component.ts
+++ front-end/src/app/pages/home/home.component.ts
@@ -22,31 +22,10 @@ export class HomeComponent implements OnInit {
   results: any;
   product: any;
 
-  products : Product[]
+  products : Product[];
   constructor(private http:HttpClient, private modalService: NgbModal, private cartService: CartService) {
 
   }
-
-  // produto = [
-  //   {
-  //     nome: "Bubaaloo Uva",
-  //     valor: "18,00",
-  //     quantidade: "50 unidades",
-  //     imagem: "bubaaoo-uva.webp"
-  //   },
-  //   {
-  //     nome: "Bala Butter Toffer",
-  //     valor: "15,00",
-  //     quantidade: "50 unidades",
-  //     imagem: "bala-recheada.webp"
-  //   },
-  //   {
-  //     nome: "Pirulito Pop",
-  //     valor: "10,00",
-  //     quantidade: "50 unidades",
-  //     imagem: "pirulito-pop.webp"
-  //   }
-  // ]
 
   ngOnInit(): void {
     this.product = this.http.get(this.list_url)
