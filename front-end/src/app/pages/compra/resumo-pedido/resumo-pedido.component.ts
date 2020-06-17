a21e31e518615e92744dbc6bbcaf51603499c078
BrunodePaula <brunodepaula18@gmail.com>
2020-06-17 17:27:29 -0300

    Update - Resumo pedido e forma pagamento

--- front-end/src/app/pages/compra/resumo-pedido/resumo-pedido.component.ts
+++ front-end/src/app/pages/compra/resumo-pedido/resumo-pedido.component.ts
@@ -9,30 +9,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./resumo-pedido.component.scss']
 })
 export class ResumoPedidoComponent implements OnInit {
-  // produtos = [
-  //   {
-  //     imagem: 'bala-recheada.webp',
-  //     nome: 'Bala Toffer',
-  //     quant: '1',
-  //     preco: '10,00',
-  //     total: '10.00'
-  //   },
-  //   {
-  //     imagem: 'pirulito-pop.webp',
-  //     nome: 'Pirulito Pop',
-  //     quant: '2',
-  //     preco: '13,00',
-  //     total: '26.00'
-  //   },
-  //   {
-  //     imagem: 'bubaaoo-uva.webp',
-  //     nome: 'Bubaaoo',
-  //     quant: '1',
-  //     preco: '15,00',
-  //     total: '15.00'
-  //   }
-  // ]
-
+ 
   constructor(private cartService: CartService, private router: Router) { }
 
   ngOnInit() {
